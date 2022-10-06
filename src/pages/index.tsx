5f1917aa73317e99b3496e6a17138107b839416f
ShamanAndrey <96531318+ShamanAndrey@users.noreply.github.com>
2022-10-06 11:58:04 +0200

    Design update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,13 +3,15 @@ import Head from "next/head";
 import { trpc } from "../utils/trpc";
 import { FaDiscord, FaTiktok, FaTwitch, FaYoutube } from "react-icons/fa";
 import { SiOnlyfans } from "react-icons/si";
-import background from "/public/bg.gif";
+import background from "/public/Rooom.png";
 import Image from "next/image";
 import { useState } from "react";
 import Throne from "/public/Thone.png";
 import Bust from "/public/Bust.png";
 import Smug from "/public/smug.png";
+import Heart from "/public/heart.png";
 import type { NextApiRequest } from "next";
+import { motion, AnimatePresence } from "framer-motion";
 
 interface props {
   ip: string;
@@ -32,23 +34,35 @@ const Home: NextPage<props> = (props) => {
       mutate({ ip: ip });
     }
 
+    const variants = {
+      open: { opacity: 1, y: "200px" },
+      closed: { opacity: 0, y: "0px" },
+    };
+
     return (
-      <div
-        className={
-          isVerificationScreenOpen
-            ? "z-20 absolute w-screen h-screen flex items-center justify-center"
-            : "hidden"
-        }
-      >
-        <div
-          className="w-screen h-screen bg-black opacity-75 absolute"
-          onClick={() => {
-            setIsVerificationScreenOpen(false);
-          }}
-        />
-        <div className="absolute z-20 md:w-[420px] w-[320px] h-44 bg-[#141414] rounded-2xl flex flex-col justify-center items-center border-2 border-gray-500 overflow-hidden">
+      <div className="flex items-center justify-center">
+        {isVerificationScreenOpen ? (
+          <motion.div
+            className="w-screen h-screen bg-black opacity-75 absolute top-0 left-0 z-20"
+            onClick={() => {
+              setIsVerificationScreenOpen(false);
+            }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 0.8 }}
+          />
+        ) : null}
+        <motion.div
+          className="absolute z-20 md:w-[420px] w-[320px] h-44 bg-[#141414] rounded-2xl flex flex-col justify-center items-center border-2 border-gray-500 overflow-hidden"
+          initial={{ opacity: 0, y: "300px" }}
+          animate={isVerificationScreenOpen ? "open" : "closed"}
+          variants={variants}
+          transition={{ duration: 0.5 }}
+        >
           <div className="absolute top-0 md:-left-12 -left-20 md:w-[200px] md:h-[200px] w-[180px] h-[180px]">
             <Image src={Smug} alt="smug face emoji" layout="fill" />
+          </div>
+          <div className="absolute top-0 md:-right-12 -right-20 md:w-[200px] md:h-[200px] w-[180px] h-[180px]">
+            <Image src={Heart} alt="smug face emoji" layout="fill" />
           </div>
           <h1 className="text-white text-2xl">18+ Content</h1>
           <a
@@ -60,7 +74,7 @@ const Home: NextPage<props> = (props) => {
           >
             <h1 className="text-white text-xl">Enter</h1>
           </a>
-        </div>
+        </motion.div>
       </div>
     );
   };
@@ -70,18 +84,18 @@ const Home: NextPage<props> = (props) => {
       <div className="absolute bottom-0 left-0 z-10 w-[200px] h-[280px] md:flex hidden">
         <Image src={Bust} alt="Bust of Silent DestroyerZ"></Image>
         <div className="absolute text-white bottom-8 left-[120px] text-6xl font-Sedgwick flex flex-col">
-          <span className="">
-            <h1 className="absolute text-[#7192ff] bottom-12 scale-105">
+          <span>
+            <h1 className="absolute text-[#fc5bff] bottom-12 scale-105">
               Silent
             </h1>
-            <h1 className="absolute text-[#ff3ed5] bottom-12">Silent</h1>
+            <h1 className="absolute text-[#ffffff] bottom-12">Silent</h1>
           </span>
-          <span className="">
-            <h1 className="absolute text-[#7192ff] bottom-0 scale-105 flex">
-              Destroyer <span className="text-[#5471fe]">Z</span>
+          <span>
+            <h1 className="absolute text-[#fc5bff] bottom-0 scale-105 flex">
+              Destroyer <span className="text-[#fc5bff]">Z</span>
             </h1>
-            <h1 className="absolute text-[#c933ff] bottom-0 flex">
-              Destroyer <span className="text-[#ff16e4]">Z</span>
+            <h1 className="absolute text-[#ffffff] bottom-0 flex">
+              Destroyer <span className="text-[#ffffff]">Z</span>
             </h1>
           </span>
         </div>
@@ -100,62 +114,69 @@ const Home: NextPage<props> = (props) => {
         <link rel="icon" href="/heart.png" />
       </Head>
 
-      <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4 font-Sedgwick bg-black">
-        <TopRigthAnalysis />
-        <SilentChatter />
-        <VerificationContainer />
-        <div className="grid gap-3 pt-3 md:mt-12 text-center md:grid-cols-2 lg:w-2/3 z-10">
-          <LinkContainer link="https://www.twitch.tv/silentdestroyerz">
-            <span className="text-[#6441a5] text-5xl pr-5">
-              <FaTwitch />
-            </span>
-            <h2 className="text-[#6441a5] font-bold text-5xl">Twitch</h2>
-          </LinkContainer>
-          <LinkContainer link="https://discord.com/invite/qRs74jU4mN">
-            <span className="text-[#5865F2] text-5xl pr-5">
-              <FaDiscord />
-            </span>
-            <h2 className="text-[#5865F2] font-bold text-5xl">Discord</h2>
-          </LinkContainer>
-          <LinkContainer link="https://www.tiktok.com/@silentdestroyerz_ttv">
-            <span className="text-white text-5xl pr-5">
-              <FaTiktok />
-            </span>
-            <h2 className="text-white font-bold text-5xl">TikTok</h2>
-          </LinkContainer>
-          <LinkContainer link="https://www.youtube.com/channel/UCx5pgI2XWXsUvjjA9TO9X6w">
-            <span className="text-red-500 text-5xl pr-5">
-              <FaYoutube />
-            </span>
-            <h2 className="text-white font-bold text-5xl">
-              You<span className="">Tube</span>
-            </h2>
-          </LinkContainer>
-          <div
-            className="btn"
-            onClick={() => setIsVerificationScreenOpen(true)}
-          >
-            <span className="text-blue-400 text-5xl pr-5">
-              <SiOnlyfans />
-            </span>
-            <h2 className="text-white font-bold text-5xl">
-              <span className="text-blue-500">Only</span>
-              <span className="text-blue-600">Fans</span>
-            </h2>
+      <main className=" container mx-auto flex flex-col items-center justify-center min-h-screen p-4 font-Sedgwick bg-black">
+        <AnimatePresence>
+          <TopRigthAnalysis />
+          <SilentChatter />
+          <VerificationContainer />
+          <div className=" grid gap-3 pt-3 md:mt-12 text-center md:grid-cols-2 lg:w-2/3 z-10">
+            <LinkContainer link="https://www.twitch.tv/silentdestroyerz">
+              <span className="text-[#6441a5] text-5xl pr-5">
+                <FaTwitch />
+              </span>
+              <h2 className="text-[#6441a5] font-bold text-5xl">Twitch</h2>
+            </LinkContainer>
+            <LinkContainer link="https://discord.com/invite/qRs74jU4mN">
+              <span className="text-[#5865F2] text-5xl pr-5">
+                <FaDiscord />
+              </span>
+              <h2 className="text-[#5865F2] font-bold text-5xl">Discord</h2>
+            </LinkContainer>
+            <LinkContainer link="https://www.tiktok.com/@silentdestroyerz_ttv">
+              <span className="text-white text-5xl pr-5">
+                <FaTiktok />
+              </span>
+              <h2 className="text-white font-bold text-5xl">TikTok</h2>
+            </LinkContainer>
+            <LinkContainer link="https://www.youtube.com/channel/UCx5pgI2XWXsUvjjA9TO9X6w">
+              <span className="text-red-500 text-5xl pr-5">
+                <FaYoutube />
+              </span>
+              <h2 className="text-white font-bold text-5xl">
+                You<span className="">Tube</span>
+              </h2>
+            </LinkContainer>
+            <div
+              className="btn"
+              onClick={() => setIsVerificationScreenOpen(true)}
+            >
+              <span className="text-blue-400 text-5xl pr-5">
+                <SiOnlyfans />
+              </span>
+              <h2 className="text-white font-bold text-5xl">
+                <span className="text-blue-500">Only</span>
+                <span className="text-blue-600">Fans</span>
+              </h2>
+            </div>
+            <LinkContainer link="https://throne.me/u/silentdestroyerz">
+              <Image
+                src={Throne}
+                alt="throne logo"
+                height={25}
+                width={65}
+              ></Image>
+              <h2 className="text-white font-bold text-5xl ml-5">Wishlist</h2>
+            </LinkContainer>
           </div>
-          <LinkContainer link="https://throne.me/u/silentdestroyerz">
+          <div className="w-screen h-screen absolute ">
             <Image
-              src={Throne}
-              alt="throne logo"
-              height={25}
-              width={65}
-            ></Image>
-            <h2 className="text-white font-bold text-5xl ml-5">Wishlist</h2>
-          </LinkContainer>
-        </div>
-        <div className="w-screen h-screen absolute ">
-          <Image src={background} alt="background" layout="fill"></Image>
-        </div>
+              src={background}
+              alt="background"
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </AnimatePresence>
       </main>
     </>
   );
